23771e6652c29770c7bfaa1b4f5fa8a5ded11eec
Ivan Siderov <ivan.siderov@corp.accedia.com>
2018-08-09 17:42:06 +0300

    refactored: post details screen

--- src/app/post-details/post-details.component.ts
+++ src/app/post-details/post-details.component.ts
@@ -8,16 +8,24 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './post-details.component.html',
   styleUrls: ['./post-details.component.scss']
 })
+
+
 export class PostDetailsComponent implements OnInit {
 
-  post$: Object;
+  author$: any
+  post$: any
 
   constructor(private data: DataService, private route: ActivatedRoute) {
     this.route.params.subscribe(params => this.post$ = params.id)
   }
 
   ngOnInit() {
-    this.data.getPost(this.post$).subscribe(data => this.post$ = data)
-  }
+    this.data.getPost(this.post$).subscribe((post: any) => {
+      this.post$ = post
 
+      this.data.getUser(post.id).subscribe(author => {
+        this.author$ = author
+      })
+    })
+  }
 }
